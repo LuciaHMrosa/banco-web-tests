e0febd166dadc8864859ec031a63de946387f354
Lucia <lhmr11@hotmail.com>
2026-04-14 14:52:56 -0300

    Adicionando testes de transferencias

---
+++ cypress/e2e/transferencia.cy.js
@@ -0,0 +1,39 @@
+describe('Transferencia', () => {
+
+    beforeEach(() => {
+        // arrange
+        cy.visit('/')
+
+        cy.fixture('credenciais').then((credenciais) => {
+            cy.get('#username').type(credenciais.valida.usuario)
+            cy.get('#senha').type(credenciais.valida.senha)
+            cy.contains('button', 'Entrar').click()
+        })
+    })
+
+    it('Deve transferir quando informo dados e valor validos', () => {
+
+        cy.get('label[for="conta-origem"]')
+            .parent()
+            .as('campo-conta-origem')
+
+        cy.get('@campo-conta-origem').click()
+        cy.get('@campo-conta-origem').contains('Maria').click()
+
+
+
+        cy.get('label[for="conta-destino"]')
+            .parent()
+            .as('campo-conta-destino')
+
+        cy.get('@campo-conta-destino').click()
+        cy.get('@campo-conta-destino').contains('João').click()
+
+        cy.get('#valor').click().type('11')
+        cy.contains('button', 'Transferir').click()
+
+        cy.get('.toast').should('have.text', 'Transferencia realizada!')
+
+    })
+
+})
